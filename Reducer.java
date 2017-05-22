54922957e9eb2f2bbb340ab846053d23e8a9a1ae
harsha1396 <vardhini1124@gmail.com>
2017-05-22 00:48:38 -0700

    Add files via upload

---
+++ Reducer.java
@@ -0,0 +1,25 @@
+TotalUnitsSoldByEachCompanyReducer.java
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class TotalUnitsSoldByEachCompanyReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+{	
+	private IntWritable total = new IntWritable();
+	private Integer minValue = Integer.MIN_VALUE;
+	
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException
+	{	
+		Integer count = 0;
+		for ( IntWritable value : values ) 
+		{	if(value.get()>minValue){
+			count+=value.get();
+		}
+		}
+		total.set(count);
+		context.write(key, total);
+	}
+}
+
